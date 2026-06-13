feecddfc5d38ac0af2352101e6824c3fb6f92179
3bd-ulrahman <abdulrahman198276301@gmail.com>
2026-06-13 15:47:37 +0300

    feat: list subscriptions

---
+++ app/Filament/Resources/Subscriptions/SubscriptionResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources\Subscriptions;
+
+use App\Filament\Resources\Subscriptions\Pages\ManageSubscriptions;
+use App\Models\Subscription;
+use BackedEnum;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SubscriptionResource extends Resource
+{
+    protected static ?string $model = Subscription::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required(),
+                Select::make('plan_id')
+                    ->relationship('plan', 'name')
+                    ->required(),
+                TextInput::make('status')
+                    ->required()
+                    ->default('active'),
+                DatePicker::make('starts_at')
+                    ->required(),
+                DatePicker::make('ends_at')
+                    ->required(),
+                DatePicker::make('cancelled_at'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user.name')
+                    ->searchable(),
+                TextColumn::make('plan.name')
+                    ->searchable(),
+                TextColumn::make('status')
+                    ->searchable(),
+                TextColumn::make('starts_at')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('ends_at')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('cancelled_at')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageSubscriptions::route('/'),
+        ];
+    }
+}
